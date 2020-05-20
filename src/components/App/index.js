f9b702eb7c429427c332d2486eaaff87a7dda751
Norberto Mantohac <nmantohac82098@gmail.com>
2020-05-19 23:42:38 -0700

    initialized repo with basic react app

---
+++ src/components/App/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import Navbar from '../Navbar';
+import Home from '../Home';
+
+const App = () => (
+  <Router>
+    <Navbar />
+    <Route exact path="/" component={Home} />
+    <Route exact path="/home" component={Home} />
+  </Router>
+);
+
+export default App;
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => (
+  <div>
+    <Link to="/home">Home</Link>
+  </div>
+);
+
+export default Navbar;
