f8f56b181b88ac039cf37cb28d0b6fc120a8326e
Norberto Mantohac <nmantohac82098@gmail.com>
2020-06-03 19:12:49 -0700

    modified about page to center content and image

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -11,26 +11,26 @@ const About = () => {
       <Helmet>
         <style>{'body { background-color: #37373b; }'}</style>
       </Helmet>
-      <Grid style={{ padding: '10em 3em' }}>
+      <Grid container centered style={{ padding: '10em 3em' }}>
         <Grid.Row>
           <Grid.Column style={{ maxWidth: 1000 }}>
             <Segment textAlign="center" style={{ marginLeft: '5em 0em', backgroundColor: 'silver' }}>
               <Header style={{ textDecoration: 'underline', fontSize: '48px' }}>About Me</Header>
               <Divider />
-              <Grid textAlign="left" verticalAlign="middle">
+              <Grid verticalAlign="middle">
                 <Grid.Row columns={2}>
-                  <Grid.Column width={4}>
-                    <Image src={pfp1} height="250" width="250" style={{ marginLeft: '20px' }} />
-                  </Grid.Column>
-                  <Grid.Column width={12}>
-                    <p style={{ fontSize: '20px' }}>I am a senior student at UC Berkeley that took a year off in order to pursue my interest in web development and programming in general.</p>
-                    <p style={{ fontSize: '20px' }}>Little did I know that my interest grew into a passion during the time I was in the the UC Berkeley Extension Web Development Bootcamp. After I graduated from the program, I have continued to sharpen my skils as a Full-Stack Developer and have been starting to look into more languages such as Java/Python/C++/etc.</p>
+                  <Grid.Column width={16}>
+                    <Grid centered>
+                      <Image src={pfp1} height="300" width="300" style={{ marginTop: '20px' }} />
+                    </Grid>
                   </Grid.Column>
                 </Grid.Row>
               </Grid>
               <Grid textAlign="left">
                 <Grid.Row columns={1}>
                   <Grid.Column width={16} style={{ marginLeft: '20px' }}>
+                    <p style={{ fontSize: '20px' }}>I am a senior student at UC Berkeley that took a year off in order to pursue my interest in web development and programming in general.</p>
+                    <p style={{ fontSize: '20px' }}>Little did I know that my interest grew into a passion during the time I was in the the UC Berkeley Extension Web Development Bootcamp. After I graduated from the program, I have continued to sharpen my skils as a Full-Stack Developer and have been starting to look into more languages such as Java/Python/C++/etc.</p>
                     <p style={{ fontSize: '20px' }}>My major was Chemical Biology in the College of Chemistry, but I plan on continuing my education at UC Berkeley by switching into Computer Science in the College of Letters & Science or the College of Electrical Engineering and Computer Sciences, and obtain a CS degree.</p>
                     <p style={{ fontSize: '20px' }}>My overall goal and career-work is Software Engineering, as well as Video Game Design & Development somewhere down the line, using game engines such as Unity and/or Unreal Engine 4. I also have interest in creating art/animation through the graphics software, Blender. I really hope that I can become a skilled game developer that has worked on major AAA titles in the future!</p>
                     <Header as="h1" style={{ fontSize: '36px' }}>Contact Info</Header>
